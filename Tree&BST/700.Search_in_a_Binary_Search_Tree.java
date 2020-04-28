985cc7b69d86e6d2adad0706138809eca93e673d
Jommy-AI <lechangzhang@gmail.com>
2020-04-28 08:22:41 +0800

    LeetcodeNO.700

---
+++ Tree&BST/700.Search_in_a_Binary_Search_Tree.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        //Just a simple binary search in BST
+        while(root != null) {
+            if(val == root.val) {
+                return root;
+            } else if (val < root.val) {
+                root = root.left;
+            } else {
+                root = root.right;
+            }
+        }
+        return null;
+    }
+}
